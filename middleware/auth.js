b3fb4601925b22cab21a80570959469912c4bd0e
wkwksama <mailpakgasing@gmail.com>
2018-09-07 21:55:33 +0700

    token ga pake bearer

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ var jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
   if (req.headers.authorization) {
-    var token = req.headers.authorization.split(' ')[1]
+    var token = req.headers.authorization
     jwt.verify(token, process.env.SECRET, function (err, decoded) {
       if (err) {
         res.status(403).json({
