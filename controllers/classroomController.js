fef22216bb15a7ed3528597a039218331184f83c
wkwksama <mailpakgasing@gmail.com>
2018-09-08 04:53:44 +0700

    validasi classroom hanya bisa satu kali dan kuota 24

--- controllers/classroomController.js
+++ controllers/classroomController.js
@@ -3,6 +3,16 @@ var Classroom = require('../models/Classroom')
 module.exports = {
   create: async function (req, res) {
     try {
+      let myClass = await Classroom.findOne({ by: req.user.sub })
+      if (myClass) {
+        throw { message: 'kamu sudah memilih kelas' }
+      }
+
+      let countClass = await Classroom.countDocuments({ className: req.body.className })
+      if (countClass > 23) {
+        throw { message: 'kuota kelas penuh', count: countClass }
+      }
+
       var classroom = await Classroom.create({
         className: req.body.className,
         by: req.user.sub,
@@ -13,7 +23,8 @@ module.exports = {
     }
     catch (err) {
       res.status(500).json({
-        message: err.message
+        message: err.message,
+        detail: err
       })
     }
   },
